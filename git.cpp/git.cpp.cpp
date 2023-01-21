15b07b99e2b73bf781138a11a3870eb093a631cb
DadTe <120330970+DadTe@users.noreply.github.com>
2023-01-21 19:03:19 +0200

    first version

--- git.cpp/git.cpp.cpp
+++ git.cpp/git.cpp.cpp
@@ -1,7 +1,17 @@
 ﻿#include <iostream>
-
+#include <Windows.h>
+#include <conio.h>
+#include <stdio.h>
+using namespace std;
+HANDLE hConsole;
+enum ColorConsole {
+    Blue = 3
+};
 int main()
 {
-    std::cout << "Hello World!\n";
+    hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole, Blue);
+    cout << "Hello World" << endl;
+    return 0;
 }
 
